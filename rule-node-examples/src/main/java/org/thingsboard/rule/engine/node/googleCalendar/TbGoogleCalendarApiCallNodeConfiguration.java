96a22e28910ed949a62953c2db2a9b63cea81315
wictor <wisv1500@student.miun.se>
2019-10-15 11:32:23 +0200

    examples UI added

--- rule-node-examples/src/main/java/org/thingsboard/rule/engine/node/googleCalendar/TbGoogleCalendarApiCallNodeConfiguration.java
+++ rule-node-examples/src/main/java/org/thingsboard/rule/engine/node/googleCalendar/TbGoogleCalendarApiCallNodeConfiguration.java
@@ -1,4 +1,23 @@
 package org.thingsboard.rule.engine.node.googleCalendar;
 
-public class TbGoogleCalendarApiCallNodeConfiguration {
+import lombok.Data;
+import org.thingsboard.rule.engine.api.NodeConfiguration;
+import java.util.Collection;
+
+
+@Data
+public class TbGoogleCalendarApiCallNodeConfiguration implements NodeConfiguration<TbGoogleCalendarApiCallNodeConfiguration>{
+
+    private String calendarUrl;
+    private String requestMethod;
+    private String serviceAccountKey;
+    private String serviceAccountKeyFileName;
+
+    @Override
+    public TbGoogleCalendarApiCallNodeConfiguration defaultConfiguration() {
+        TbGoogleCalendarApiCallNodeConfiguration configuration = new TbGoogleCalendarApiCallNodeConfiguration();
+        configuration.setCalendarUrl("Calendar Url");
+        configuration.setRequestMethod("GET");
+        return configuration;
+    }
 }
